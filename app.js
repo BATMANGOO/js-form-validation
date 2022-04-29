36e36cb61051f17eec5c46020ce4cccb1641b289
Prabhjot Singh <pjsingh1359@gmail.com>
2022-04-28 19:49:55 -0700

    add validation and styles

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const country = document.querySelector('#country');
 const zipCode = document.querySelector('#zip');
 const password = document.querySelector('#password');
 const passwordConfirm = document.querySelector('#password_confirmation');
+const errors = document.querySelector('.errors');
+
+const errorArray = [];
+
+email.required = true;
+zipCode.required = true;
+country.required = true;
+password.required = true;
+passwordConfirm.required = true;
 
 email.addEventListener('input', e => {
-  if (email.validity.typeMismatch) {
+  if (email.validity.typeMismatch || email.value.length <= 0) {
     email.setCustomValidity('Please enter a valid email address');
     email.reportValidity();
   } else {
@@ -35,18 +44,61 @@ password.addEventListener('input', e => {
   }
 });
 
-form.addEventListener('submit', e => {
-  if (password.value !== passwordConfirm.value) {
-    e.preventDefault();
+passwordConfirm.addEventListener('input', e => {
+  if (passwordConfirm.value !== password.value) {
     passwordConfirm.setCustomValidity('Passwords do not match');
     passwordConfirm.reportValidity();
   } else {
     passwordConfirm.setCustomValidity('');
-    form.submit();
+  }
+});
+
+form.addEventListener('submit', e => {
+  if (email.validity.typeMismatch || email.value.length <= 0) {
+    e.preventDefault();
+    recieveError('Please enter a valid email address');
+  }
+
+  if (password.value !== passwordConfirm.value) {
+    e.preventDefault();
+    recieveError('Passwords do not match');
   }
 
   if (email.value === "" || country.value === "Select a country" || zipCode.value === "" || password.value === "" || passwordConfirm.value === "") {
     e.preventDefault();
-    alert('Please fill in all fields');
+    recieveError('Please fill out all fields');
   }
+
+  if (zipCode.value.length < 5 || !zipCode.value.match(/^[0-9]+$/)) {
+    e.preventDefault();
+    recieveError('Please enter a valid zip code');
+  }
+
+  if (password.validity.tooLong || password.validity.tooShort) {
+    e.preventDefault();
+    recieveError('Password must be between 5 and 20 characters');
+  } else if (password.validity.valueMissing) {
+    e.preventDefault();
+    recieveError('Password is required');
+  }
+
+  if (passwordConfirm.value !== password.value) {
+    e.preventDefault();
+    recieveError('Passwords do not match');
+  }
+
+  displayErrors();
 });
+
+function recieveError(error) {
+  if (errorArray.includes(error)) {
+    return;
+  } 
+  errorArray.push(error);
+}
+
+const arr = ['hello world'];
+
+function displayErrors() {
+  errors.innerHTML = errorArray.map(error => `<li>${error}</li>`).join('');
+}
